96ed1b7a23c5215ed8f8f777ba088c0659b52446
smitbry17 <smitbry17@gmail.com>
2023-02-01 17:45:57 -0800

    wrote out all category routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+
 });
 
 // create new product
